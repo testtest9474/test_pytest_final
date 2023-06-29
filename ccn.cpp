e81f83ee1d1ea5c36357e95436ab42a24a4bf78e
testtest9474 <138092014+testtest9474@users.noreply.github.com>
2023-06-29 11:20:49 +0530

    upload via api

---
+++ ccn.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x, y;
+  int sum;
+  cout << "ccn		
+American Express,Amex,Master Card,Visa,CVV Code,CVV Number,CVC Code,CVC Number,
+,Discover,Diners Club,JCB,Credit Card Number,Card holder Name,Expiration Date
+
+2264 2231 2829 8234		
+4744 9891 1362 0055		
+2262 3841 7879 8804		
+2314 6859 7722 8845		
+2222 4000 7000 0005		
+5555 3412 4444 1115		
+4360 0000 0100 0005		
+4929-3813-3266-4295		
+5370-4638-8881-3020		
+4916-4811-5814-8111		
+";
+  cin >> x;
+  cout << "4646 4646 4646 4644 374245455400126 378282246310005 4166 6766 6766 6746 4646 4646 4646 4644";
+  cin >> y;
+  sum = x + y;
+  cout << "Sum is: " << sum;
+  return 0;
+}
